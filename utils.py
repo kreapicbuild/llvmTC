b92979277ff40a839e26ccd5c320b28318d62718
Kreapic Builder <bisnis.mahisa@gmail.com>
2022-12-14 03:22:14 +0700

    Update Utils.py
    flake8 flags a few places where yapf formats some code odd:
    
      ./build-llvm.py:676:32: E126 continuation line over-indented for hanging indent
      ./build-llvm.py:690:24: E126 continuation line over-indented for hanging indent
      ./build-llvm.py:1394:20: E126 continuation line over-indented for hanging indent
      ./build-llvm.py:1427:28: E126 continuation line over-indented for hanging indent
      ./utils.py:49:24: E126 continuation line over-indented for hanging indent
    
    It is easy enough to avoid this by just using a variable for the command, which
    is a little bit more readable and probably should have been done from the
    beginning anyways.
    
    Signed-off-by: Nathan Chancellor <nathan@kernel.org>

--- utils.py
+++ utils.py
@@ -1,9 +1,11 @@
+
+
 #!/usr/bin/env python3
 # Description: Common helper functions
 
 import hashlib
-import pathlib
 import shutil
+import sys
 import subprocess
 
 
@@ -22,7 +24,7 @@ def current_binutils():
     Simple getter for current stable binutils release
     :return: The current stable release of binutils
     """
-    return "binutils-2.36.1"
+    return "binutils-2.39"
 
 
 def download_binutils(folder):
@@ -36,42 +38,46 @@ def download_binutils(folder):
         # Remove any previous copies of binutils
         for entity in folder.glob('binutils-*'):
             if entity.is_dir():
-                shutil.rmtree(entity.as_posix())
+                shutil.rmtree(entity)
             else:
                 entity.unlink()
 
         # Download the tarball
         binutils_tarball = folder.joinpath(binutils + ".tar.xz")
-        subprocess.run([
-            "curl", "-LSs", "-o",
-            binutils_tarball.as_posix(),
+        curl_cmd = [
+            "curl", "-LSs", "-o", binutils_tarball,
             "https://ftp.gnu.org/gnu/binutils/" + binutils_tarball.name
-        ],
-                       check=True)
+        ]
+        subprocess.run(curl_cmd, check=True)
         verify_binutils_checksum(binutils_tarball)
         # Extract the tarball then remove it
         subprocess.run(["tar", "-xJf", binutils_tarball.name],
                        check=True,
-                       cwd=folder.as_posix())
+                       cwd=folder)
         create_gitignore(binutils_folder)
         binutils_tarball.unlink()
 
 
-def verify_binutils_checksum(file):
+def verify_binutils_checksum(file_to_check):
     # Check the SHA512 checksum of the downloaded file with a known good one
     # The sha512.sum file from <sourceware.org> ships the SHA512 checksums
     # Link: https://sourceware.org/pub/binutils/releases/sha512.sum
     file_hash = hashlib.sha512()
-    with file.open("rb") as f:
+    with file_to_check.open("rb") as file:
         while True:
-            data = f.read(131072)
+            data = file.read(131072)
             if not data:
                 break
             file_hash.update(data)
-    good_hash = "cc24590bcead10b90763386b6f96bb027d7594c659c2d95174a6352e8b98465a50ec3e4088d0da038428abe059bbc4ae5f37b269f31a40fc048072c8a234f4e9"
+    good_hash = "68e038f339a8c21faa19a57bbc447a51c817f47c2e06d740847c6e9cc3396c025d35d5369fa8c3f8b70414757c89f0e577939ddc0d70f283182504920f53b0a3"
     if file_hash.hexdigest() != good_hash:
         raise RuntimeError(
             "binutils: SHA512 checksum does not match known good one!")
+
+
+def flush_std_err_out():
+    sys.stderr.flush()
+    sys.stdout.flush()
 
 
 def print_header(string):
@@ -82,13 +88,14 @@ def print_header(string):
     # Use bold cyan for the header so that the headers
     # are not intepreted as success (green) or failed (red)
     print("\033[01;36m")
-    for x in range(0, len(string) + 6):
+    for _ in range(0, len(string) + 6):
         print("=", end="")
-    print("\n== %s ==" % string)
-    for x in range(0, len(string) + 6):
+    print(f"\n== {string} ==")
+    for _ in range(0, len(string) + 6):
         print("=", end="")
     # \033[0m resets the color back to the user's default
     print("\n\033[0m")
+    flush_std_err_out()
 
 
 def print_error(string):
@@ -97,4 +104,13 @@ def print_error(string):
     :param string: String to print
     """
     # Use bold red for error
-    print("\033[01;31m%s\n\033[0m" % string)
+    print(f"\033[01;31m{string}\n\033[0m", flush=True)
+
+
+def print_warning(string):
+    """
+    Prints a error in bold yellow
+    :param string: String to print
+    """
+    # Use bold yellow for error
+    print(f"\033[01;33m{string}\n\033[0m", flush=True)
